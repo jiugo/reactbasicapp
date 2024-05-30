cb6e7191856db609b7f8bbe8672a85c59e7b6986
Hugo Olivera Alonso <hugo.olivera@gmail.com>
2024-05-30 19:54:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hola gente del Taller {value}</div>;
+  const value = 'Taller';
+  return <div>Hola gente del  {value}</div>;
 }
 
 export default App;
