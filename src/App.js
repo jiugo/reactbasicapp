ad329101d50a16e1256bd2c8fe8e238816764e30
Hugo Olivera Alonso <hugo.olivera@gmail.com>
2025-05-20 20:18:26 -0300

    Update App.js
    cambio de anio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'Taller de Deploy 2024';
+  const value = 'Taller de Deploy 2025';
   return <div>Hola gente que esta en el  {value}</div>;
 }
 
