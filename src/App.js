79a8595a6ee416c3ed3de6d93ac3c528903ea644
Hugo Olivera Alonso <hugo.olivera@gmail.com>
2025-05-20 21:02:52 -0300

    Update App.js
    new update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'Taller de Deploy 2025 Demos';
+  const value = 'Taller de Deploy 2025 Demos Martes';
   return <div>Hola gente que esta en el  {value}</div>;
 }
 
