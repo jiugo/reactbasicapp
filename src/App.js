e102074f5ca8289dad8d9dfdba8117ebcb09d022
Hugo Olivera Alonso <hugo.olivera@gmail.com>
2026-05-26 20:52:48 -0300

    nuevo update de la app
    version 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'Taller de Deploy 2026 Demos de Martes 26 Mayo - Version Dev 2.0';
+  const value = 'Taller de Deploy 2026 Demos de Martes 26 Mayo - Version Dev 3.0';
   return <div>Hola gente que esta en el  {value}</div>;
 }
 
