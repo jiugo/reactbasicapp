91e8d3af23cc5e4dc389b39088b28dd153d64fc8
Hugo Olivera Alonso <hugo.olivera@gmail.com>
2026-05-26 20:04:56 -0300

    update de la app
    cambios en el mensaje

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'Taller de Deploy Demos de Martes 26 - Version Main';
+  const value = 'Taller de Deploy Demos del 2026 de Martes 26 - Version Main';
   return <div>Hola gente que esta en el  {value}</div>;
 }
 
