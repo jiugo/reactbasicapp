600ff6b49eaa20770a86a6fc5dca8e3da4e7a481
Hugo Olivera Alonso <hugo.olivera@gmail.com>
2026-06-02 20:08:57 -0300

    Update version string in App component
    nueov update del mensaje para v1.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'Taller de Deploy Demos del 2026 de Martes 2 - Version Main';
+  const value = 'Taller de Deploy Demos del 2026 de Martes 2 - Version Main 1.0';
   return <div>Hola gente que esta en el  {value}</div>;
 }
 
