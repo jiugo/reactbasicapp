9040febb070e1b53b5f4e665a10c741d6af37126
Hugo Olivera Alonso <hugo.olivera@gmail.com>
2026-06-02 20:13:16 -0300

    Update App.js
    udpate de mensaje a version 1.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'Taller de Deploy Demos del 2026 de Martes 2 - Version Main 1.0';
+  const value = 'Taller de Deploy Demos del 2026 de Martes 2 - Version Main 1.1';
   return <div>Hola gente que esta en el  {value}</div>;
 }
 
